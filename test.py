f2cdb5421fe6e673a717fbebcfec6e82fa36c3dd
dsp <cybrshin@gmail.com>
2018-06-30 01:54:55 +0900

    add test

--- test.py
+++ test.py
@@ -20,8 +20,9 @@ def print_json_data_list(json_list):
   print('==============================')
 
 
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(level=logging.DEBUG)
 # upbitpy = Upbitpy('input access key', 'input secret')
+upbitpy = Upbitpy()
 
 ###############################################################
 # EXCHANGE API
@@ -65,25 +66,62 @@ logging.basicConfig(level=logging.DEBUG)
 ###############################################################
 
 # get market all
-# print_json_data_list(upbitpy.get_market_all())
+#print_json_data_list(upbitpy.get_market_all())
 
 # get candles - minutes
-# print_json_data_list(upbitpy.get_minutes_candles(240, 'KRW-QTUM'))
+#print_json_data_list(upbitpy.get_minutes_candles(240, 'KRW-QTUM'))
 
 # get candles - days
-# print_json_data_list(upbitpy.get_days_candles('KRW-QTUM', count=10))
+#print_json_data_list(upbitpy.get_days_candles('KRW-QTUM', count=10))
 
 # get candles - weeks
-# print_json_data_list(upbitpy.get_weeks_candles('KRW-EOS'))
+#print_json_data_list(upbitpy.get_weeks_candles('KRW-EOS'))
 
 # get candles - month
 # print_json_data_list(upbitpy.get_months_candles('KRW-BTC'))
 
 # get trades ticks
-# print_json_data_list(upbitpy.get_trades_ticks('KRW-QTUM'))
+#print_json_data_list(upbitpy.get_trades_ticks('KRW-QTUM'))
 
 # get ticker
-# print_json_data_list(upbitpy.get_ticker(['KRW-BTC', 'BTC-ETH']))
+#print_json_data_list(upbitpy.get_ticker(['KRW-BTC']))
 
 # get orderbook
 # print_json_data_list(upbitpy.get_orderbook(['KRW-QTUM', 'BTC-ETH']))
+
+import requests
+from forex_python.converter import CurrencyRates
+
+c = CurrencyRates()
+
+def get_kope(coinName="BTC"):
+  ticker = upbitpy.get_ticker(['KRW-'+coinName.upper()])
+  btckrw_upbit = float( ticker[0]['trade_price'] )
+  print( 'upbit',coinName,'krw :', btckrw_upbit )
+
+  r = requests.get(url='https://api.bitfinex.com/v1/ticker/'+coinName.lower()+'usd')
+  btcusd = float( r.json()['last_price'] )
+
+  print( 'bitfinex',coinName,'usd :', btcusd )
+
+  usdkrw = float( c.get_rate('USD', 'KRW') )
+  print( 'usdkrw :', usdkrw )
+
+  btckrw_bitfinex = btcusd * usdkrw
+  print( 'bitfinex',coinName,'krw :' , btckrw_bitfinex )
+
+  kope = (btckrw_upbit / btckrw_bitfinex) - 1
+  print( coinName,'kope :', kope )
+
+  return kope
+
+get_kope("btc")
+#get_kope("eth")
+#get_kope("xrp")
+#get_kope("eos")
+#get_kope("trx")
+#get_kope("xlm")
+#get_kope("snt")
+get_kope("neo")
+
+
